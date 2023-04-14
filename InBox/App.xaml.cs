eb0930a9d6432d60366c506ba0b44a2d92b421b2
HiroHsu <hiro@hirohsu.info>
2023-04-14 22:42:19 +0800

    先建立一個預設範本

---
+++ InBox/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace InBox;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ InBox/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace InBox;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
